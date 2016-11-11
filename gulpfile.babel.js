f3ae5e3d82747303d22ac5f08f5774c63c4c954e
Uday Shivamurthy <uday.shivamurthy@gmail.com>
2016-11-11 21:54:18 +1100

    hatha nov 2016 email

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -12,7 +12,7 @@ import siphon   from 'siphon-media-query';
 const $ = plugins();
 const center = 'au';
 const year = '2016';
-const wip = 'sathsang-201611';
+const wip = 'hatha-201611';
 const wip_path = center + '/' + year + '/' + wip + '.html';
 const base = 'src/pages/';
 const src = base + wip_path;
