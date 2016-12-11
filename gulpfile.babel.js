357fb53538d61f899bfd65c76b60275ff4e200b4
Uday Shivamurthy <uday.shivamurthy@gmail.com>
2016-12-11 22:59:14 +1100

    initial draft of monthly news letter

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -12,7 +12,7 @@ import siphon   from 'siphon-media-query';
 const $ = plugins();
 const center = 'au';
 const year = '2016';
-const wip = 'sathsang-201612';
+const wip = 'MonthlyEmail';
 const wip_path = center + '/' + year + '/' + wip + '.html';
 const base = 'src/pages/';
 const src = base + wip_path;
